0617d2f20fde6098de7622c7765406985493ad00
Mohammad Gheysari <mohammadgheysari79.dev@gmail.com>
2024-04-11 17:12:22 +0330

    Add background color

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const MaterialColor primry = Colors.blue;
+  static const MaterialColor primary = Colors.blue;
+  static const Color background = Color(0xFFEEEEEE);
 }
