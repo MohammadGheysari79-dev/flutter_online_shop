f11b6a4cccf9bc1c6aca86b516d0651d18a1fab4
Mohammad Gheysari <mohammadgheysari79.dev@gmail.com>
2024-04-21 11:34:37 +0330

    Implement category model

---
+++ lib/data_layer/model/category.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class Category {
+  String name;
+  Color color;
+  String imageAddress;
+
+  Category({
+    required this.name,
+    required this.color,
+    required this.imageAddress,
+  });
+}
